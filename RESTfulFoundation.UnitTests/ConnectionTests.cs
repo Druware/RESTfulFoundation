9d10e9e8c32e3ad3494d2fe993ff8f5706c5e1dc
DruSatori <dru@druware.com>
2023-12-27 21:29:26 -0500

    Completed transition of unittests, all passing

--- RESTfulFoundation.UnitTests/ConnectionTests.cs
+++ RESTfulFoundation.UnitTests/ConnectionTests.cs
@@ -85,7 +85,7 @@ namespace RESTfulFoundation.UnitTests
             var connection = new RESTConnection(_configuration.Host);
             Assert.That(connection, Is.Not.Null, "Connection should never be null");
 
-            var result = await connection.ListAsync<Player>(Player.Path, "");
+            var result = await connection.ListAsync<Player>(Player.Path);
 
             Assert.That(result, Is.Not.EqualTo(null), "Result does not contain a list");
             Assert.That(result!.Count, Is.GreaterThan(0), "List does not have any records");
@@ -119,188 +119,181 @@ namespace RESTfulFoundation.UnitTests
             Assert.That(result.TotalRecords ?? 0, Is.GreaterThan(0), "List does not have any records");        }
 
         // GET/POST/PUT/DELETE
-/*
+
         [Test]
         public void TestGet()
         {
-            string RootPath = "https://www.trustee13.com/";
-            string Path = "/unittest/api/Players";
+            Assert.That(_configuration.Host, Is.Not.Null, "Host Cannot be null");
 
-            RESTConnection connection = new(RootPath);
+            var connection = new RESTConnection(_configuration.Host);
+            Assert.That(connection, Is.Not.Null, "Connection should never be null");
 
-            Player? result = connection.Get<Player?>(Path, (1).ToString());
+            var result = connection.Get<Player>(Player.Path, 1.ToString());
             Assert.Multiple(() =>
             {
                 Assert.That(result?.PlayerId, Is.EqualTo(1), "Result Id does not match");
                 Assert.That(result?.PlayerName, Is.EqualTo("Mickey Mouse"), "Result Name does not match");
             });
-            return;
         }
-
+        
         [Test]
         public async Task TestGetAsync()
         {
-            string RootPath = "https://www.trustee13.com/";
-            string Path = "/unittest/api/Players";
+            Assert.That(_configuration.Host, Is.Not.Null, "Host Cannot be null");
 
-            RESTConnection connection = new(RootPath);
+            var connection = new RESTConnection(_configuration.Host);
+            Assert.That(connection, Is.Not.Null, "Connection should never be null");
 
-            Player? result = await connection.GetAsync<Player?>(Path, (1).ToString());
+            var result = await connection.GetAsync<Player>(Player.Path, 1.ToString());
             Assert.Multiple(() =>
             {
                 Assert.That(result?.PlayerId, Is.EqualTo(1), "Result Id does not match");
                 Assert.That(result?.PlayerName, Is.EqualTo("Mickey Mouse"), "Result Name does not match");
             });
-            return;
         }
 
         [Test]
         public void TestPost()
         {
-            string RootPath = "https://www.trustee13.com/";
-            string Path = "/unittest/api/Players";
+            Assert.That(_configuration.Host, Is.Not.Null, "Host Cannot be null");
 
-            RESTConnection connection = new(RootPath);
-
+            var connection = new RESTConnection(_configuration.Host);
+            Assert.That(connection, Is.Not.Null, "Connection should never be null");
+            
             Player p = new() { PlayerName = "A Test of Post" };
 
-            Player? result = connection.Post<Player?>(Path, p);
+            var result = connection.Post<Player, Player>(Player.Path, p);
             Assert.Multiple(() =>
             {
                 Assert.That(result?.PlayerId, Is.Not.EqualTo(null), "Result Id is null");
                 Assert.That(result?.PlayerName, Is.EqualTo("A Test of Post"), "Result Name does not match");
 
-                Assert.That(connection.Delete(Path, result!.PlayerId.ToString()),
+                Assert.That(connection.Delete(Player.Path, result!.PlayerId.ToString()),
                     Is.True, "Delete failed");
             });
-            return;
         }
 
         [Test]
         public async Task TestPostAsync()
         {
-            string RootPath = "https://www.trustee13.com/";
-            string Path = "/unittest/api/Players";
+            Assert.That(_configuration.Host, Is.Not.Null, "Host Cannot be null");
 
-            RESTConnection connection = new(RootPath);
+            var connection = new RESTConnection(_configuration.Host);
+            Assert.That(connection, Is.Not.Null, "Connection should never be null");
+            
+            Player p = new() { PlayerName = "A Test of Post" };
 
-            Player p = new() { PlayerName = "An Async Test of Post" };
-
-            Player? result = await connection.PostAsync<Player?>(Path, p);
+            var result = await connection.PostAsync<Player, Player>(Player.Path, p);
             Assert.Multiple(() =>
             {
                 Assert.That(result?.PlayerId, Is.Not.EqualTo(null), "Result Id is null");
-                Assert.That(result?.PlayerName, Is.EqualTo("An Async Test of Post"), "Result Name does not match");
+                Assert.That(result?.PlayerName, Is.EqualTo("A Test of Post"), "Result Name does not match");
 
-                Assert.That(connection.Delete(Path, result!.PlayerId.ToString()),
+                Assert.That(connection.Delete(Player.Path, result!.PlayerId.ToString()),
                     Is.True, "Delete failed");
-            });
-            return;
+            });        
         }
+        
 
         [Test]
         public void TestPut()
         {
-            string RootPath = "https://www.trustee13.com/";
-            string Path = "/unittest/api/Players";
+            Assert.That(_configuration.Host, Is.Not.Null, "Host Cannot be null");
 
-            RESTConnection connection = new(RootPath);
+            var connection = new RESTConnection(_configuration.Host);
+            Assert.That(connection, Is.Not.Null, "Connection should never be null");
 
             Player p = new() { PlayerId = 2, PlayerName = "An Async Test of Put" };
 
-            Player? result = connection.Put<Player?>(Path, 2.ToString(), p);
-            result ??= connection.Get<Player?>(Path, 2.ToString());
+            var result = connection.Put<Player?, Player>(Player.Path, 2.ToString(), p);
+            result ??= connection.Get<Player?>(Player.Path, 2.ToString());
             Assert.Multiple(() =>
             {
                 Assert.That(result?.PlayerId, Is.Not.EqualTo(null), "Result Id is null");
                 Assert.That(result?.PlayerName, Is.EqualTo("An Async Test of Put"), "Result Name does not match");
             });
             result!.PlayerName = "Donald Duck";
-            result = connection.Put<Player?>(Path, 2.ToString(), result);
-            result ??= connection.Get<Player?>(Path, 2.ToString());
+            result = connection.Put<Player?, Player>(Player.Path, 2.ToString(), result);
+            result ??= connection.Get<Player?>(Player.Path, 2.ToString());
 
             Assert.Multiple(() =>
             {
                 Assert.That(result?.PlayerId, Is.Not.EqualTo(null), "Result Id is null");
                 Assert.That(result?.PlayerName, Is.EqualTo("Donald Duck"), "Result Name does not match");
             });
-            return;
         }
 
         [Test]
         public async Task TestPutAsync()
         {
-            string RootPath = "https://www.trustee13.com/";
-            string Path = "/unittest/api/Players";
+            Assert.That(_configuration.Host, Is.Not.Null, "Host Cannot be null");
 
-            RESTConnection connection = new(RootPath);
+            var connection = new RESTConnection(_configuration.Host);
+            Assert.That(connection, Is.Not.Null, "Connection should never be null");
 
             Player p = new() { PlayerId = 2, PlayerName = "An Async Test of Put" };
 
-            Player? result = await connection.PutAsync<Player?>(Path, 2.ToString(), p);
-            result ??= await connection.GetAsync<Player?>(Path, 2.ToString());
+            var result = await connection.PutAsync<Player?, Player>(Player.Path, 2.ToString(), p);
+            result ??= await connection.GetAsync<Player?>(Player.Path, 2.ToString());
             Assert.Multiple(() =>
             {
                 Assert.That(result?.PlayerId, Is.Not.EqualTo(null), "Result Id is null");
                 Assert.That(result?.PlayerName, Is.EqualTo("An Async Test of Put"), "Result Name does not match");
             });
             result!.PlayerName = "Donald Duck";
-            result = await connection.PutAsync<Player?>(Path, 2.ToString(), result);
-            result ??= await connection.GetAsync<Player?>(Path, 2.ToString());
+            result = await connection.PutAsync<Player?, Player>(Player.Path, 2.ToString(), result);
+            result ??= await connection.GetAsync<Player?>(Player.Path, 2.ToString());
+
             Assert.Multiple(() =>
             {
                 Assert.That(result?.PlayerId, Is.Not.EqualTo(null), "Result Id is null");
                 Assert.That(result?.PlayerName, Is.EqualTo("Donald Duck"), "Result Name does not match");
-            });
-            return;
+            });        
         }
 
         [Test]
         public void TestPostDelete()
         {
-            string RootPath = "https://www.trustee13.com/";
-            string Path = "/unittest/api/Players";
+            Assert.That(_configuration.Host, Is.Not.Null, "Host Cannot be null");
 
-            RESTConnection connection = new(RootPath);
+            var connection = new RESTConnection(_configuration.Host);
+            Assert.That(connection, Is.Not.Null, "Connection should never be null");
 
             Player p = new() { PlayerName = "A Test of Post" };
 
-            Player? result = connection.Post<Player?>(Path, p);
+            var result = connection.Post<Player?, Player>(Player.Path, p);
             Assert.Multiple(() =>
             {
                 Assert.That(result?.PlayerId, Is.Not.EqualTo(null), "Result Id is null");
                 Assert.That(result?.PlayerName, Is.EqualTo("A Test of Post"), "Result Name does not match");
 
-                Assert.That(connection.Delete(Path, result!.PlayerId.ToString()),
+                Assert.That(connection.Delete(Player.Path, result!.PlayerId.ToString()),
                     Is.True, "Delete failed");
             });
-            return;
         }
 
         [Test]
         public async Task TestPostDeleteAsync()
         {
-            string RootPath = "https://www.trustee13.com/";
-            string Path = "/unittest/api/Players";
+            Assert.That(_configuration.Host, Is.Not.Null, "Host Cannot be null");
 
-            RESTConnection connection = new(RootPath);
+            var connection = new RESTConnection(_configuration.Host);
+            Assert.That(connection, Is.Not.Null, "Connection should never be null");
 
             Player p = new() { PlayerName = "A Test of Post" };
 
-            Player? result = await connection.PostAsync<Player?>(Path, p);
-            Assert.Multiple(async () =>
+            var result = await connection.PostAsync<Player?, Player>(Player.Path, p);
+            Assert.Multiple(() =>
             {
                 Assert.That(result?.PlayerId, Is.Not.EqualTo(null), "Result Id is null");
                 Assert.That(result?.PlayerName, Is.EqualTo("A Test of Post"), "Result Name does not match");
-
-
-                Assert.That(await connection.DeleteAsync(Path, result!.PlayerId.ToString()),
-                    Is.True, "Delete failed");
             });
-            return;
+            var deleted = await connection.DeleteAsync(Player.Path,
+                result!.PlayerId.ToString());
+            Assert.That(deleted, Is.True, "Delete failed");
         }
         
-        */
+        
     }
     
 }
